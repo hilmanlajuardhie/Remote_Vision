cebab8c529465ba50de41d1268aebb06bc5ecbff
Papa <hilmanlajuardhie@gmail.com>
2026-08-27 14:34:11 +0700

    ...

--- src/Capture_device.py
+++ src/Capture_device.py
@@ -9,23 +9,23 @@ def Record():
     print(f"Running YOLO11 on device: {device}")
     model = YOLO("model/yolo11m.pt")
 
-    cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture(0, cv.CAP_V4L2)
     if not cap.isOpened():
         print("Error: Could not open camera.")
         return
 
     cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, 720)
-    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
     cap.set(cv.CAP_PROP_FPS, 60)
 
-    dummy_input = np.zeros((480, 720, 3), dtype=np.uint8)
+    dummy_input = np.zeros((720, 1280, 3), dtype=np.uint8)
     for _ in range(3):
         model.predict(
             source=dummy_input, 
             device=device, 
             verbose=False, 
-            quantize=32
+            quantize=16
             )
 
     frame_count = 0
@@ -38,6 +38,14 @@ def Record():
             if not ret:
                 break
 
+            results = model.predict(
+                source=frame, 
+                device=device, 
+                verbose=False, 
+                quantize=16
+                )
+            annotated_frame = results[0].plot()
+            
             frame_count += 1
             current_time = time.time()
             elapsed_time = current_time - last_time
@@ -49,17 +57,8 @@ def Record():
                 print(f"FPS: {fps_display:.2f}")
 
             fps_text = f"FPS:{fps_display:.2f}"
-            cv.putText(frame, fps_text, (10, 20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (235,150,0), 2, cv.LINE_AA)
+            cv.putText(annotated_frame, fps_text, (10, 20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (235,150,0), 2, cv.LINE_AA)
             print(f"FPS: {fps_display:.2f}")
-
-            results = model.predict(
-                source=frame, 
-                device=device, 
-                verbose=False, 
-                quantize=32
-                )
-
-            annotated_frame = results[0].plot()
 
             cv.imshow("Record: YOLOv11", annotated_frame)
             if cv.waitKey(1) & 0xFF == ord('q'):
